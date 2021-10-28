fa364d30a9af35cf68886d846d033fa1bbd8f6bd
MarcosADA <90805616+MarcosADA@users.noreply.github.com>
2021-10-28 14:49:50 -0500

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'dart:async';
+import 'dart:typed_data';
 import 'package:flutter/material.dart'; //Base
 import 'package:location/location.dart'; //Locacion
 import 'package:flutter/foundation.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 import 'dart:io';
 import 'dart:convert';
-import 'package:intl/intl.dart'; //cambio de formato
+import 'package:permission_handler/permission_handler.dart' as hd;
+
 
 void main() {
   runApp(MyApp());
@@ -39,13 +42,19 @@ class _MyHomePageState extends State<MyHomePage> {
   bool TaxiSwitch = false;
   int Carro=1;
   Timer Bucle =Timer.periodic(Duration(seconds: 1000000), (Timer t) => null);
+  String Mensaje="";
+
 
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title), ),
+        title: Text(widget.title),
+        actions: <Widget>[
+          new IconButton(onPressed: _pushBluetooth, icon: const Icon(Icons.list)),
+        ],
+      ),
       body: cuerpo(), );
   }
 
@@ -107,6 +116,25 @@ class _MyHomePageState extends State<MyHomePage> {
               activeColor: Colors.blue,
             ),
 
+            Card(
+                color: Colors.grey,
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text('$Mensaje',style: TextStyle(fontSize:5),),
+                ),
+            ),
+            Row(
+              children: [
+                TextButton(
+                  style: ButtonStyle(
+                    foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                  ),
+                  onPressed: () {Conectar("");},
+                  child: Text('Conectar OBD2'),
+                ),
+
+              ],
+            ),
           ],
           mainAxisAlignment: MainAxisAlignment.center,
 
@@ -114,6 +142,8 @@ class _MyHomePageState extends State<MyHomePage> {
       ],mainAxisAlignment: MainAxisAlignment.center,
     );
   }
+
+
 
 
   _sendLocation() async{
@@ -161,5 +191,89 @@ class _MyHomePageState extends State<MyHomePage> {
       udpSocket.send(utf8.encode(_Mensaje),IP2.first,8050);
     });
   }
+
+  void _pushBluetooth(){
+    Navigator.of(context).push(
+      new MaterialPageRoute<void>(
+        builder: (BuildContext context){
+          return new Scaffold(
+            appBar: new AppBar(
+              title: const Text('Bluetooth'),
+            ),
+            body: new Card(
+              color: Colors.grey,
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('$Mensaje',style: TextStyle(fontSize:21),),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Conectar(String data) async{
+    BluetoothState _bluetoothState = BluetoothState.UNKNOWN;
+    _bluetoothState = await FlutterBluetoothSerial.instance.state;
+    FlutterBluetoothSerial bluetooth = FlutterBluetoothSerial.instance;
+    BluetoothDevice _device;
+    List<BluetoothDevice> devices = [];
+
+    if (await hd.Permission.bluetooth.request().isGranted) {
+    }
+
+    if (_bluetoothState == BluetoothState.STATE_OFF) {
+      await FlutterBluetoothSerial.instance.requestEnable();
+      return true;
+    }
+
+    try {
+      print("Try to connected");
+
+      Mensaje="Buscando dispositivo...";
+      devices = await bluetooth.getBondedDevices();
+      print(devices.length);
+      print(devices);
+      for (int i=0;i<devices.length;i++){
+        print(i);
+        print(devices[i].name);
+        if (devices[i].name=="OBDII"){
+          _device=devices[i];
+          Mensaje="Conectando con OBDII "+_device.address+"...";
+          BluetoothConnection connection = await BluetoothConnection.toAddress(_device.address);
+
+
+          print('Connected to the device'+_device.address);
+          Mensaje="Conectado con OBDII "+_device.address;
+          if(connection != null){
+            connection.output.add(Uint8List.fromList(utf8.encode("ATZ \r\n")));
+            await connection.output.allSent;
+            connection.output.add(Uint8List.fromList(utf8.encode("ATSP0 \r\n")));
+            await connection.output.allSent;
+            connection.output.add(Uint8List.fromList(utf8.encode("ATDP \r\n")));
+            await connection.output.allSent;
+            connection.output.add(Uint8List.fromList(utf8.encode("01 OC \r\n")));
+            await connection.output.allSent;
+
+            }
+          Mensaje="Enviando mensaje";
+          Mensaje="";
+          connection.input!.listen((Uint8List data) {
+            //Data entry point
+            Mensaje=Mensaje+(ascii.decode(data));
+          });
+          connection.close();
+        }
+      }
+      if (Mensaje=="Buscando dispositivo..."){Mensaje="No se encontro el OBDII";}
+
+    }
+    catch (exception) {
+      print('Cannot connect, exception occured'+exception.toString());
+      Mensaje="Error al conectar /r/n"+exception.toString();
+    }
+  }
+
 
 }
